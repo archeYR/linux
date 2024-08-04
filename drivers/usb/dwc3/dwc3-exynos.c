dfa8e9aafd04f672482cc952c4f743ffe612a0e5
David Virag <virag.david003@gmail.com>
2024-08-04 17:53:01 +0200

    usb: dwc3: exynos: Add support for Exynos7885
    Add Exynos7885 compatible string. As 7885 has the same clocks as 850, it
    can reuse the data for Exynos850.
    
    Signed-off-by: David Virag <virag.david003@gmail.com>

--- drivers/usb/dwc3/dwc3-exynos.c
+++ drivers/usb/dwc3/dwc3-exynos.c
@@ -210,6 +210,9 @@ static const struct of_device_id exynos_dwc3_match[] = {
 		.compatible = "samsung,exynos7870-dwusb3",
 		.data = &exynos7870_drvdata,
 	}, {
+		.compatible = "samsung,exynos7885-dwusb3",
+		.data = &exynos850_drvdata,
+	}, {
 		.compatible = "samsung,exynos850-dwusb3",
 		.data = &exynos850_drvdata,
 	}, {
